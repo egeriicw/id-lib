81611c408d2b4ef04e757d930f9935cdac5fdc95
Bill Eger <egeriicw@gmail.com>
2013-03-11 17:43:59 -0400

    recoded csv input function, recoded csv and json writing functions, cleaned up code.

--- src/intervaldata.py
+++ src/intervaldata.py
@@ -1,60 +1,214 @@
+#! /usr/bin/env python
+
 import csv
 import datetime as dt
+import json
 
 class IntervalData:
+    #
+    #
+    #
+    #
 
-    def __init__(self, filename):
+    filename = ""
+    idata = list()
+    fdataList = list()
+    fdataDict = dict()
+    dataframedata = ""
+    mDim = ""
+    nDim = ""
+    cnames = ""
+    shape = ""
+    headerset = False
+    headerrow = list()
+
+
+    def __init__(self, filename=None):
+        #
+        #
+        #
+        #
+        
+        print "__Init__"
+        self.idata = list()
+        self.fdataList = list()
+        self.fdataDict = dict()
+        self.headerrow = list()
+        self.headerset = False
         self.filename = filename
-        self.idata = []
-        self.fdata = []
-        self.mDim = 0
-        self.nDim = 0
-        self.cnames = ["Date","12:00 AM","12:30 AM", "1:00 AM","1:30 AM","2:00 AM","2:30 AM","3:00 AM","3:30 AM","4:00 AM","4:30 AM","5:00 AM","5:30 AM","6:00 AM","6:30 AM","7:00 AM","7:30 AM","8:00 AM","8:30 AM","9:00 AM","9:30 AM","10:00 AM","10:30 AM","11:00 AM","11:30 AM","12:00 PM","12:30 PM","1:00 PM","1:30 PM","2:00 PM","2:30 PM","3:00 PM","3:30 PM","4:00 PM","4:30 PM","5:00 PM","5:30 PM","6:00 PM","6:30 PM","7:00 PM","7:30 PM","8:00 PM","8:30 PM","9:00 PM","9:30 PM","10:00 PM","10:30 PM","11:00 PM","11:30 PM"]
- 
         
-    def getIntervalData(self):
-        return self.fdata
 
-    def read_csv(self):
-        print "read_csv"
-        with open(self.filename, 'rb') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                self.idata.append(list(row))
-        self.idata = map(list, zip(*self.idata))
-        self.mDim = len(self.idata)
-        self.nDim = len(self.idata[1])
-    
-    def write_csv(self):
-        with open( "../data/output/testoutput.csv", 'wb') as f:
-            writer = csv.writer(f, delimiter=',', dialect='excel')
-            for row in self.fdata:
-                writer.writerow(row)
-        f.close()
+    def setFilename(self, filename):
+        self.filename = filename
 
-    def print_csv(self):
-        print "print_csv"
-        
-    def printParameters(self):
-        print "Columns: ", self.mDim
-        print "Rows: ", self.nDim
-        
+
+    def read_CSV(self, filename=None, header=None):
+
+        #
+        #
+        #
+        #
+
+        print "read_CSV"
+
+        if filename != None or self.filename != None:
+
+            # What happens if both filenames are different
+            # self.filename takes precedence
+
+            location = filename or self.filename
+            print "Location: ", location
+
+            try:
+                print "trying"
+                with open(location, 'rb') as infile:
+                    print "File: ", self.filename, " Open."
+                    if header == None:
+                        for line in infile:
+                                self.idata.append(line.strip('\r\n').split(','))
+                    else:
+                        for line in infile:
+                            if self.headerset == False:
+                                self.headerrow = line.strip('\r\n').split(',')
+                                self.headerset = True
+                            else:
+                                self.idata.append(line.strip('\r\n').split(','))
+
+            except IOError as e:
+                print "IOException: ", e
+            finally:
+                infile.close()
+        else:
+            print "Requires a filename."
+
+
     def melt(self):
-        transposed = zip(*self.idata)
-        
-        for trans in range(0, len(transposed)):
-            temp = transposed[trans]
-            for t in range(1, len(self.cnames)):
-                datetime_str = temp[0] + " " + self.cnames[t]
-                datetimes = dt.datetime.strptime(datetime_str, "%m/%d/%Y %H:%M %p")
-                self.fdata.append([datetimes, temp[t]])
+        print "...Melt..."
 
+        #
+        #
+        #
+        #
+        
+        for i in range(0,len(self.idata)):
+            for j in range(1, len(self.idata[i])):
+            
+               # Create List Representation
+                datetime_str = self.idata[i][0] + " " + self.headerrow[j]
+                datetimes = dt.datetime.strptime(datetime_str, "%m/%d/%Y %I:%M %p")
+                self.fdataList.append([datetimes, self.idata[i][j]])
+
+                # Create Dict Representation
+                self.fdataDict.setdefault(self.idata[i][0], {})
+                self.fdataDict[self.idata[i][0]][dt.datetime.strptime(self.headerrow[j], "%I:%M %p").strftime("%H:%M")] = self.idata[i][j] 
+
+        print "Melt Complete."
+                
+    def write_JSON(self, filename=None):
+        #
+        #
+        #
+        #
+
+        print "Writing to JSON."
+
+        if filename != None:
+            try:
+                with open( "../data/output/" + filename, 'wb') as outfile:
+                    outfile.write(json.dumps(self.fdataDict, sort_keys=True, indent=4))
+            except IOError as e:
+                print "IOException: ", e
+            finally:
+                outfile.close()
+                print "JSON saved to ", filename
+        else:
+            print "Output JSON file name required."
+        
+    def write_CSV(self, filename=None):
+        #
+        #
+        #
+        #
+
+        print "Writing to CSV."
+
+        if filename != None:
+            try:
+                with open( "../data/output/" + filename, 'wb') as outfile:
+                    writer = csv.writer(outfile, delimiter=',', dialect='excel')
+                    for row in self.fdataList:
+                        writer.writerow(row)
+            except IOError as e:
+                print "IOException: ", e
+            finally:
+                outfile.close()
+                print "CSV saved to ", filename
+
+        else:
+            print "Output CSV file name required."
+
+    def getDataList(self):
+        #
+        #
+        #
+        #
+
+        print "Get Data"
+        
+##        if type(datatype) == list():
+##            print "list"
+##            return self.fdataList
+##        elif type(datatype) == dict:
+##            return self.fdataDict
+##        else:
+##            return 0
+
+        return self.fdataList
+
+    def getDataDict(self):
+        #
+        #
+        #
+        #
+
+        return self.fdataDict
+
+        
+
+
+##    def createDataFrame(self):
+##        print "...Create Data Frame..."
+##        self.dataframedata = pd.DataFrame(self.fdata)
+##        print "Columns: ", self.dataframedata.columns[0], self.dataframedata.columns[1]
+##        self.dataframedata.columns = ["Date", "Value"]
+##        print "Columns: ", self.dataframedata.columns[0], self.dataframedata.columns[1]
+##        print "Index: ", self.dataframedata.index
+##        print "Date: ", self.dataframedata["Date"]
+##        print "Values: ", self.dataframedata["Value"]
+##
+##    def sort(self):
+##        print "...Sort..."
+##        return 0
+##
+##    def getShape(self):
+##        print "...getShape..."
+##        return 0
+##    
+##    def getFFT(self):
+##        # return FFT of interval data
+##        return 0
+        
 def main():
-    filename = "../data/input/ckid.csv"
-    d = IntervalData(filename)
-    d.read_csv()
+    name = "../data/input/ckid.csv"
+    d = IntervalData(name)
+    d.read_CSV(name, header=True)
     d.melt()
-    d.write_csv()
+    d.write_CSV("csvtest.csv")
+    d.write_JSON("jsontest.json")
+    test = d.getDataList()
+
+    print test[0:10000]
+
     
 
 if __name__ == "__main__":
